fdc07283f98f11d278ab278409c9183f50b281c2
Fakhrul Siddiqei <fakhrulasa@gmail.com>
2023-03-17 16:58:04 +0600

    init

---
+++ app/src/main/java/com/famstudio/tiktok/ui/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.famstudio.tiktok.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.famstudio.tiktok.R
+import com.famstudio.tiktok.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var bottomNavigationView: BottomNavigationView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navView: BottomNavigationView = binding.bottom
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        navView.setupWithNavController(navController)
+    }
+}
