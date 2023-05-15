45212043ae55322d56a16fe5d5eab85cd84e602d
Fakhrul Siddiqei <fakhrul.antor@upaybd.com>
2023-05-15 15:23:56 +0600

    download

--- app/src/main/java/com/famstudio/tiktok/ui/MainActivity.kt
+++ app/src/main/java/com/famstudio/tiktok/ui/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.famstudio.tiktok.ui
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.GravityCompat
 import androidx.core.view.WindowCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupWithNavController
 import com.famstudio.tiktok.R
 import com.famstudio.tiktok.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,11 +25,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         var drawerLayout: DrawerLayout = binding.drawerLayout
         binding.imageView4.setOnClickListener {
-            if (drawerLayout.isDrawerOpen((GravityCompat.START))) {
-                drawerLayout.closeDrawer(GravityCompat.START);
-            } else {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
+
+            drawerLayout.openDrawer(GravityCompat.START)
+
+        }
+        val nav_view : NavigationView = binding.navView
+        val premimumbtn: ConstraintLayout = nav_view.getHeaderView(0).findViewById(R.id.nav_premium)
+        premimumbtn.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            startActivity(Intent(this, PremiumPackageActivity::class.java))
         }
     }
 
---
+++ app/src/main/java/com/famstudio/tiktok/ui/PremiumPackageActivity.kt
@@ -0,0 +1,20 @@
+package com.famstudio.tiktok.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.famstudio.tiktok.R
+import com.famstudio.tiktok.databinding.ActivityMainBinding
+import com.famstudio.tiktok.databinding.ActivityPremiumPackageBinding
+
+class PremiumPackageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPremiumPackageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPremiumPackageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.imageView10.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+}
